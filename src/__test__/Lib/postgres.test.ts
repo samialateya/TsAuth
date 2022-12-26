819912d10a11c94099f0f5e9d6172dd4ba578a62
Sami <sami@audiomob.com>
2022-12-26 14:30:31 +0400

    force exit the test

--- src/__test__/Lib/postgres.test.ts
+++ src/__test__/Lib/postgres.test.ts
@@ -10,12 +10,6 @@ describe('Check the postgres functionality', () => {
 	it('should be able to connect to postgres client', async () => {
 		// wait for the postgres module to be connected
 		const postgresModule = await PostgresModule;
-		// print the environment variables
-		console.log('host', process.env.POSTGRES_HOST);
-		console.log('port', process.env.POSTGRES_PORT);
-		console.log('user', process.env.POSTGRES_USER);
-		console.log('password', process.env.POSTGRES_PASSWORD);
-		console.log('database', process.env.POSTGRES_DATABASE);
 		// check if the postgres client is connected
 		expect(postgresModule.client._connected).toBe(true);
 	});
