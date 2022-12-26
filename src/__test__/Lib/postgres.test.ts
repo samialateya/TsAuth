3ad1e84e674a64b38dbf3401659dd465ef1e3025
Sami <sami@audiomob.com>
2022-12-26 13:48:31 +0400

    test for github workflow env vairables

--- src/__test__/Lib/postgres.test.ts
+++ src/__test__/Lib/postgres.test.ts
@@ -1,4 +1,7 @@
 import PostgresModule from '../../Lib/Postgres';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 /* ------------------------ test postgres functionality ------------------------ */
 describe('Check the postgres functionality', () => {
@@ -7,6 +10,12 @@ describe('Check the postgres functionality', () => {
 	it('should be able to connect to postgres client', async () => {
 		// wait for the postgres module to be connected
 		const postgresModule = await PostgresModule;
+		// print the environment variables
+		console.log('host', process.env.POSTGRES_HOST);
+		console.log('port', process.env.POSTGRES_PORT);
+		console.log('user', process.env.POSTGRES_USER);
+		console.log('password', process.env.POSTGRES_PASSWORD);
+		console.log('database', process.env.POSTGRES_DATABASE);
 		// check if the postgres client is connected
 		expect(postgresModule.client._connected).toBe(true);
 	});
